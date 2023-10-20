1a524c122a1369cd017c8d2cd80f799e0b66f548
Helia Ghahraman <76126890+helia-ghahraman@users.noreply.github.com>
2023-10-20 12:33:18 +0330

    Update client.cpp

--- CPP implementation/client.cpp
+++ CPP implementation/client.cpp
@@ -1,8 +1,7 @@
+#include <cpp_redis/cpp_redis>
 #include <iostream>
 #include <chrono>
 #include <random>
-#include <thread>
-#include <sw/redis++/redis++.h>
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
@@ -13,21 +12,26 @@ int main(int argc, char* argv[]) {
     std::string client_id = argv[1];
     std::string message = argv[2];
 
-    sw::redis::Redis client("tcp://127.0.0.1:6379"); // Create a Redis client
+    cpp_redis::client client;
+
+    client.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::connect_state status) {
+        if (status == cpp_redis::connect_state::dropped) {
+            std::cerr << "Client disconnected from " << host << ":" << port << std::endl;
+        }
+    });
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(1000, 5000);
-    std::string channel = "chatroom";
-    std::string message1 = client_id + ": " + message;
+    std::uniform_int_distribution<> dis(1000, 5000); // Adjust the range as needed
 
     while (true) {
-        client.publish(channel, message1); // Publish the message
-
+        client.publish("chatroom", client_id + ": " + message);
+        client.sync_commit();
         std::cout << client_id + ": " + message << std::endl;
         int sleep_duration = dis(gen); // Generate a random sleep duration
         std::this_thread::sleep_for(std::chrono::milliseconds(sleep_duration));
     }
 
-    return 0;
+
+
 }
